a2f5757519bd15a84ef0c38be01d1d227e0276dc
rightyeon <bacchus.xd@gmail.com>
2021-06-16 17:20:47 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({name, pic}) { 
+function Food({name, pic, rating}) { 
   // return <h1>I LIKE { props.fav }</h1>;
   // 구조 분해 할당 (destructuring assignment) 으로 사용하기
   return (
     <div>
       <h2>I LIKE {name}</h2>
+      <h4>{rating}/⭐️5.0</h4>
       <img src={pic} width="50%" alt={name} />
     </div>
   );
@@ -16,18 +18,26 @@ const foodILike = [
     id:1,
     name : 'Kimchi',
     image: 'https://www.canceranswer.co.kr/news/photo/202011/1492_2385_3537.jpg',
+    rating : 4
   },
   {
     id:2,
     name : 'topokki',
     image: 'https://cdnweb01.wikitree.co.kr/webdata/editor/202007/07/img_20200707090644_7ebc248e.webp',
+    rating : 3.9
   },
   {
     id:3,
     name : 'potato',
     image: 'https://img-cf.kurly.com/shop/data/goodsview/20201222/gv30000145401_1.jpg',
+    rating:4.5
   }
 ];
+
+
+Food.propTypes = {
+  
+}
 
 
 // App 컴포넌트 정의
@@ -36,9 +46,11 @@ function App() {
   return  (
     <div>
       <h1>Hello</h1>
-      { foodILike.map(dish=>(
-        <Food name={dish.name} pic={dish.image} key={dish.id} />
-      ))}
+      {
+        foodILike.map(dish=>(
+          <Food name={dish.name} pic={dish.image} key={dish.id} rating={dish.rating} />
+        ))
+      }
     </div>
   );
 }
